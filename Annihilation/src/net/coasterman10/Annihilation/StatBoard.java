581595da1c3d4df48eee7e6ede470dadafadb7a8
coasterman10 <christian.s.farrell@gmail.com>
2013-12-21 02:58:21 -0500

    fixed scoreboard

--- Annihilation/src/net/coasterman10/Annihilation/StatBoard.java
+++ Annihilation/src/net/coasterman10/Annihilation/StatBoard.java
@@ -10,43 +10,43 @@ import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.ScoreboardManager;
 
 public class StatBoard {
-    private final Scoreboard board;
-    private final Objective objective;
-    private final HashMap<String, Integer> stats;
+	private final Scoreboard board;
+	private final Objective objective;
+	private final HashMap<String, Integer> stats;
 
-    public StatBoard(ScoreboardManager manager) {
-	board = manager.getNewScoreboard();
-	objective = board.registerNewObjective("obj", "dummy");
-	stats = new HashMap<String, Integer>();
-    }
-
-    public void setTitle(String title) {
-	objective.setDisplayName(title);
-    }
-
-    public void setScore(String name, int score) {
-	stats.put(name, score);
-	objective.getScore(Bukkit.getOfflinePlayer(name)).setScore(score);
-    }
-
-    public int getScore(String name) {
-	if (stats.containsKey(name))
-	    return stats.get(name);
-	else
-	    return 0;
-    }
-    
-    public void show() {
-	objective.setDisplaySlot(DisplaySlot.SIDEBAR);
-    }
-    
-    public void hide() {
-	board.clearSlot(DisplaySlot.SIDEBAR);
-    }
-    
-    public void showForPlayers(Player... players) {
-	for (Player p : players) {
-	    p.setScoreboard(board);
+	public StatBoard(ScoreboardManager manager) {
+		board = manager.getNewScoreboard();
+		objective = board.registerNewObjective("obj", "dummy");
+		stats = new HashMap<String, Integer>();
 	}
-    }
+
+	public void setTitle(String title) {
+		objective.setDisplayName(title);
+	}
+
+	public void setScore(String name, int score) {
+		stats.put(name, score);
+		objective.getScore(Bukkit.getOfflinePlayer(name)).setScore(score);
+	}
+
+	public int getScore(String name) {
+		if (stats.containsKey(name))
+			return stats.get(name);
+		else
+			return 0;
+	}
+
+	public void show() {
+		objective.setDisplaySlot(DisplaySlot.SIDEBAR);
+	}
+
+	public void hide() {
+		board.clearSlot(DisplaySlot.SIDEBAR);
+	}
+
+	public void showForPlayers(Player... players) {
+		for (Player p : players) {
+			p.setScoreboard(board);
+		}
+	}
 }
--- Annihilation/src/net/coasterman10/Annihilation/maps/VotingManager.java
+++ Annihilation/src/net/coasterman10/Annihilation/maps/VotingManager.java
@@ -14,67 +14,69 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 
 public class VotingManager implements Listener {
-    private final StatBoard statBoard;
-    private final HashSet<String> maps = new HashSet<String>();
-    private final HashMap<String, String> votes = new HashMap<String, String>();
+	private final StatBoard statBoard;
+	private final HashSet<String> maps = new HashSet<String>();
+	private final HashMap<String, String> votes = new HashMap<String, String>();
 
-    public VotingManager(Annihilation plugin) {
-	plugin.getCommand("vote").setExecutor(new VoteCommand(this));
+	public VotingManager(Annihilation plugin) {
+		plugin.getCommand("vote").setExecutor(new VoteCommand(this));
 
-	String title = ChatColor.DARK_AQUA + "" + ChatColor.BOLD + "Voting";
-	statBoard = new StatBoard(plugin.getServer().getScoreboardManager());
-	statBoard.setTitle(title);
+		String title = ChatColor.DARK_AQUA + "" + ChatColor.BOLD + "Voting";
+		statBoard = new StatBoard(plugin.getServer().getScoreboardManager());
+		statBoard.setTitle(title);
 
-	for (String map : plugin.getMapManager().getRandomMaps()) {
-	    maps.add(map);
-	    statBoard.setScore(map, 0);
+		for (String map : plugin.getMapManager().getRandomMaps()) {
+			maps.add(map);
+			statBoard.setScore(map, 0);
+		}
+		
+		statBoard.show();
 	}
-    }
 
-    public void setCurrentForPlayers(Player... players) {
-	statBoard.showForPlayers(players);
-    }
-
-    public boolean vote(CommandSender voter, String vote) {
-	for (String map : maps) {
-	    if (vote.equalsIgnoreCase(map)) {
-		votes.put(voter.getName(), map);
-		statBoard.setScore(map, countVotes(map));
-		voter.sendMessage(ChatColor.GOLD + "You voted for "
-			+ ChatColor.WHITE + map);
-		return true;
-	    }
+	public void setCurrentForPlayers(Player... players) {
+		statBoard.showForPlayers(players);
 	}
-	voter.sendMessage(vote + ChatColor.RED + " is not a valid map");
-	return false;
-    }
 
-    public String getWinner() {
-	String winner = null;
-	Integer highest = -1;
-	for (String map : maps) {
-	    int totalVotes = countVotes(map);
-	    if (totalVotes > highest) {
-		winner = map;
-		highest = totalVotes;
-	    }
+	public boolean vote(CommandSender voter, String vote) {
+		for (String map : maps) {
+			if (vote.equalsIgnoreCase(map)) {
+				votes.put(voter.getName(), map);
+				statBoard.setScore(map, countVotes(map));
+				voter.sendMessage(ChatColor.GOLD + "You voted for "
+						+ ChatColor.WHITE + map);
+				return true;
+			}
+		}
+		voter.sendMessage(vote + ChatColor.RED + " is not a valid map");
+		return false;
 	}
-	return winner;
-    }
 
-    public void end() {
-	statBoard.hide();
-    }
+	public String getWinner() {
+		String winner = null;
+		Integer highest = -1;
+		for (String map : maps) {
+			int totalVotes = countVotes(map);
+			if (totalVotes > highest) {
+				winner = map;
+				highest = totalVotes;
+			}
+		}
+		return winner;
+	}
 
-    public Set<String> getMaps() {
-	return maps;
-    }
+	public void end() {
+		statBoard.hide();
+	}
 
-    private int countVotes(String map) {
-	int total = 0;
-	for (String vote : votes.values())
-	    if (vote.equals(map))
-		total++;
-	return total;
-    }
+	public Set<String> getMaps() {
+		return maps;
+	}
+
+	private int countVotes(String map) {
+		int total = 0;
+		for (String vote : votes.values())
+			if (vote.equals(map))
+				total++;
+		return total;
+	}
 }
