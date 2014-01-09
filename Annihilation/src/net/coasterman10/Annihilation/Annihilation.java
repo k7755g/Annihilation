e7c3f6cdb7201510ff74993c2a4d38b16241087c
Luke Anderson (stuntguy3000) <stuntguy3000@gmail.com>
2014-01-09 22:29:18 +1030

    Not sure if issue, commenting.

--- Annihilation/src/net/coasterman10/Annihilation/Annihilation.java
+++ Annihilation/src/net/coasterman10/Annihilation/Annihilation.java
@@ -225,12 +225,12 @@ public final class Annihilation extends JavaPlugin {
 	}
 
 	public void startGame() {
-		for (Player p : Bukkit.getOnlinePlayers()) {
+		/*for (Player p : Bukkit.getOnlinePlayers()) {
 			for (Player pp : Bukkit.getOnlinePlayers()) {
 				p.showPlayer(pp);
 				pp.showPlayer(p);
 			}
-		}
+		}*/
 		
 		Bukkit.getPluginManager().callEvent(
 				new GameStartEvent(maps.getCurrentMap()));
