88870d5ab64c2f7ce4b0c732d6b60b6d3958e826
Luke Anderson (stuntguy3000) <stuntguy3000@gmail.com>
2014-01-10 00:36:19 +1030

    Adding to last commit

---
+++ Annihilation/src/net/coasterman10/Annihilation/commands/DistanceCommand.java
@@ -0,0 +1,61 @@
+package net.coasterman10.Annihilation.commands;
+
+import net.coasterman10.Annihilation.Annihilation;
+import net.coasterman10.Annihilation.AnnihilationTeam;
+import net.coasterman10.Annihilation.PlayerMeta;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class DistanceCommand implements CommandExecutor {
+	private Annihilation plugin;
+
+	public DistanceCommand(Annihilation instance) {
+		this.plugin = instance;
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label,
+			String[] args) {
+		if (sender instanceof Player) {
+			Player p = (Player) sender;
+			
+			if (plugin.getPhase() == 0) {
+				p.sendMessage(ChatColor.RED + "The game hasn't started yet!");
+				return false;
+			}
+			
+			if (PlayerMeta.getMeta(p).getTeam() == AnnihilationTeam.NONE) {
+				p.sendMessage(ChatColor.RED + "The have to be ingame to use this command!");
+				return false;
+			}
+			
+			p.sendMessage(ChatColor.GRAY + "=========[ " + ChatColor.DARK_AQUA.toString() + "Distances"
+				+ ChatColor.GRAY + " ]=========");
+			
+			for (AnnihilationTeam t : AnnihilationTeam.values()) {
+				if (t != AnnihilationTeam.NONE) {
+					showTeam(p, t);
+				}
+			}
+			
+			p.sendMessage(ChatColor.GRAY + "============================");
+		} else {
+			sender.sendMessage(ChatColor.RED + "Can only be run by a player!");
+		}
+
+		return true;
+	}
+
+	private void showTeam(Player p, AnnihilationTeam t) {
+		try {
+			if (t.getNexus() != null && t.getNexus().getHealth() > 0)
+				p.sendMessage(t.coloredName() + ChatColor.GRAY + " Nexus Distance: " + ChatColor.WHITE + ((int) p.getLocation().distance(t.getNexus().getLocation())) + ChatColor.GRAY + " Blocks");
+		} catch (IllegalArgumentException ex) {
+			
+		}
+	}
+}
